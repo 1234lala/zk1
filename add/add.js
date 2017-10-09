d35bff43693297de58ec5c422bfafdd251539319
unknown <m18211060989@163.com>
2017-10-09 09:36:11 +0800

    这是js的合并

---
+++ add/add.js
@@ -0,0 +1,22 @@
+;(function(){
+
+    var arr = [1,2,2,4,6,6,6,8,9,9,9,9];
+    var tempt = [];
+    for( var i = 0; i < arr.length; i++) {
+        if (tempt.indexOf(arr[i])==-1){
+            tempt.push(arr[i]);
+        }
+    }
+     console.log(tempt);
+})()
+;(function(){
+
+    var arr = [1,2,2,4,6,6,6,8,9,9,9,9];
+    var tempt = [];
+    for( var i = 0; i < arr.length; i++) {
+        if (tempt.indexOf(arr[i])==-1){
+            tempt.push(arr[i]);
+        }
+    }
+     console.log(tempt);
+})()
